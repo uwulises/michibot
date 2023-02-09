a9826109e2662c6999824ddf2e34336bbd8c3f59
uwulises <ulises.campodonico@ug.uchile.cl>
2023-02-08 23:01:17 -0300

    update servo class

--- src/control/servo.py
+++ src/control/servo.py
@@ -1,6 +1,7 @@
 from machine import Pin, PWM
 
-
+'''Clase Servo requiere que al menos se defina un pin con capacidad PWM, pensada para controlar servos SG90 y MG90s
+Parametros: pin (GPIO), frecuencia del PWM, duty cycle min y max'''
 class Servo:
     def __init__(self, pin: int, freq=50, min=2500, max=8000):
         gpio = Pin(pin, Pin.OUT)
@@ -14,5 +15,5 @@ class Servo:
 
     def set_angle(self, angle: int):
         # map the angle (0-180) to a duty cycle
-        duty = int(2500 + (angle / 180) * (self.maxfreq-self.minfreq))
+        duty = int(self.minfreq + (angle / 180) * (self.maxfreq-self.minfreq))
         self.pwm_obj.duty_u16(duty)
